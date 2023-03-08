17c873054581bc58701f80fd0ced73a5202064d3
Barleen <barleenkaur65@gmail.com>
2023-03-08 14:26:46 +0530

    First Page of Train Booking

---
+++ src/components/train1.jsx
@@ -0,0 +1,51 @@
+import './train1.css'
+
+export default function Train1() {
+    return (
+        <div class="image">
+
+            <div class="button1">
+                <form>
+                    <input type="submit" value="Live Status">
+                        <input type="submit" value="PNR Check">
+                        </form>
+                    </div>
+                    <h1>booknow<span>.com</span></h1>
+                    <div class="button2">
+                        <form>
+                            <input type="submit" value="By Station">
+                                <input type="submit" value="By Train">
+                                </form>
+                            </div>
+                            <div class="info">
+                                <div class="details">
+                                    <div class="From">
+                                        From
+                                        <form> <input type="text"></form>
+
+                                    </div>
+
+                                    <div class="To">
+                                        To
+                                        <form><input type="text"></form>
+
+                                    </div>
+
+                                    <div class="DD">
+                                        Departure Date
+                                        <form><input type="text"></form>
+
+                                    </div>
+                                </div>
+
+                                <div class="Search">
+                                    <form>
+                                        <input type="button" value="Search Train">
+                                    </form>
+                                </div>
+                            </div>
+
+
+                    </div>
+                    )
+}
