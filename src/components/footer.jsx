79893485a427670871abdb6f9aaeed29e1849889
Sajal <sajalkhawas8@gmail.com>
2023-03-08 23:08:30 +0530

    Changed class to className in footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,29 +4,29 @@ import './footer.css'
 export default function Footer() {
     return (
         <footer>
-            <div class="foot">
-                <div class="left_box">
-                    <div class="logo">Booknow<span>.com</span></div>
+            <div className="foot">
+                <div className="left_box">
+                    <div className="logo">Booknow<span>.com</span></div>
                     <ul>
                         <li><a href="#">About</a></li>
                         <li><a href="#services">Services</a></li>
                         <li><a href="#contactUs">Contact Us </a></li>
                     </ul>
                 </div>
-                <div class="centre_box">
+                <div className="centre_box">
                     <ul>
                         <li>&copy; Copyright 2023</li>
                         <li>All Rights Reserved</li>
                     </ul>
                 </div>
-                <div class="right_box">
+                <div className="right_box">
 
                     <form action="#">
                         Sign up for updates
-                        <div class="text">Email*<input type="email" required /><br /></div>
-                        <div class="button"><input type="submit" value="Subscribe" id="button" /></div>
+                        <div className="text">Email*<input type="email" required /><br /></div>
+                        <div className="button"><input type="submit" value="Subscribe" id="button" /></div>
                     </form>
-                    <div class="social">
+                    <div className="social">
                         <a href="#"><FaFacebookSquare /></a>
                         <a href="#"><FaTwitterSquare /></a>
                         <a href="#"><FaLinkedin /></a>
