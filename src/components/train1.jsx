f36048f797865e14036378c8d8ac10db5dc9ff12
Sajal <sajalkhawas8@gmail.com>
2023-03-08 20:32:01 +0530

    Fixed some syntax errors

--- src/components/train1.jsx
+++ src/components/train1.jsx
@@ -2,50 +2,41 @@ import './train1.css'
 
 export default function Train1() {
     return (
-        <div class="image">
-
-            <div class="button1">
+        <div className="image">
+            <div className="button1">
                 <form>
-                    <input type="submit" value="Live Status">
-                        <input type="submit" value="PNR Check">
-                        </form>
+                    <input type="submit" value="Live Status" />
+                    <input type="submit" value="PNR Check" />
+                </form>
+            </div>
+            <h1>booknow<span>.com</span></h1>
+            <div className="button2">
+                <form>
+                    <input type="submit" value="By Station" />
+                    <input type="submit" value="By Train" />
+                </form>
+            </div>
+            <div className="info">
+                <div className="details">
+                    <div className="From">
+                        From
+                        <form> <input type="text" /></form>
                     </div>
-                    <h1>booknow<span>.com</span></h1>
-                    <div class="button2">
-                        <form>
-                            <input type="submit" value="By Station">
-                                <input type="submit" value="By Train">
-                                </form>
-                            </div>
-                            <div class="info">
-                                <div class="details">
-                                    <div class="From">
-                                        From
-                                        <form> <input type="text"></form>
-
-                                    </div>
-
-                                    <div class="To">
-                                        To
-                                        <form><input type="text"></form>
-
-                                    </div>
-
-                                    <div class="DD">
-                                        Departure Date
-                                        <form><input type="text"></form>
-
-                                    </div>
-                                </div>
-
-                                <div class="Search">
-                                    <form>
-                                        <input type="button" value="Search Train">
-                                    </form>
-                                </div>
-                            </div>
-
-
+                    <div className="To">
+                        To
+                        <form><input type="text" /></form>
                     </div>
-                    )
+                    <div className="DD">
+                        Departure Date
+                        <form><input type="date" /></form>
+                    </div>
+                </div>
+                <div className="Search">
+                    <form>
+                        <input type="button" value="Search Train" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
