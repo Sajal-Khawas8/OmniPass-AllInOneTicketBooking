181aa6c3ccac09ffb8caca71842fbf2aa4c314ef
Sajal <sajalkhawas8@gmail.com>
2023-04-12 23:49:26 +0530

    Email sending system is now working

--- server.cjs
+++ server.cjs
@@ -59,87 +59,17 @@ app.post("/capture-payment/:paymentId", async (req, res) => {
   }
 });
 
-// async function sendEmail(response) {
-//   // Create a nodemailer transporter with your email provider's SMTP settings
-//   let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD,
-//     },
-//   });
-
-//   // Convert the logo image to a PDFMake-compatible JSON object
-//   const logoData = htmlToPdfmake(`<img src="./src/images/AppLogo.png"></img>`);
-
-//   // Define the PDF document structure
-//   const docDefinition = {
-//     content: [
-//       // Add background image
-//       {
-//         image: logoData,
-//         width: '100%',
-//         height: '100%',
-//         absolutePosition: { x: 0, y: 0 }
-//       },
-//       // Add logo on top right corner
-//       {
-//         image: logoData,
-//         width: 50,
-//         height: 50,
-//         absolutePosition: { x: 520, y: 20 }
-//       },
-
-//       // Add invoice details
-//       { text: 'Invoice', style: 'header', alignment: 'center' },
-//       { text: 'Invoice Number:', style: 'subheader', margin: [0, 80, 0, 0] },
-//       { text: response.id, margin: [90, 80, 0, 0] },
-//       { text: 'Date:', style: 'subheader', margin: [0, 10, 0, 0] },
-//       { text: Date().substring(0, 24), margin: [90, 10, 0, 0] },
-//       { text: 'Amount:', style: 'subheader', margin: [0, 10, 0, 0] },
-//       { text: `${response.currency} ${((response.amount) / 100)}`, margin: [90, 10, 0, 0] }
-//     ],
-//     styles: {
-//       header: {
-//         fontSize: 22,
-//         bold: true,
-//         margin: [0, 0, 0, 20]
-//       },
-//       subheader: {
-//         fontSize: 16,
-//         bold: true,
-//         margin: [0, 0, 0, 5]
-//       }
-//     }
-//   };
-
-//   const pdf = await pdfMake.createPdf(docDefinition).getBuffer();
-
-//   // Send the email with the PDF file attached
-//   try {
-//     await transporter.sendMail({
-//       from: "gouravkhawas@gmail.com", // replace with your email address
-//       to: response.email, // replace with the recipient's email address
-//       subject: "Ticket cum Invoice",
-//       text: "Dear Sir/Madam,\nThanks for booking your ticket on OmniPass.\nPlease find attached your Ticket cum Invoice.\nThanks & Regards\nOmniPass",
-//       attachments: [{ filename: 'Ticket.pdf', content: pdf }],
-//     });
-//   } catch (error) {
-
-//   }
-
-// }
-
 async function sendEmail(response) {
   try {
     // Create a nodemailer transporter with your email provider's SMTP settings
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.ethereal.email',
+      port: 587,
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+          user: 'fred.quitzon@ethereal.email',
+          pass: '92cseTXvXkQs3USDs2'
+      }
+  });
 
     // Convert the logo image to a PDFMake-compatible JSON object
     // const logoData = htmlToPdfmake(`<img src="./src/images/AppLogo.png"></img>`);
@@ -184,19 +114,21 @@ async function sendEmail(response) {
         }
       }
     };
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    const fileName = `Ticket-${response.id}.pdf`;
 
-    const pdf = await pdfMake.createPdf(docDefinition).getBuffer();
-
-    // Send the email with the PDF file attached
-    const info = await transporter.sendMail({
-      from: process.env.EMAIL_USERNAME,
-      to: response.email,
-      subject: 'Ticket cum Invoice',
-      text: 'Dear Sir/Madam,\nThanks for booking your ticket on OmniPass.\nPlease find attached your Ticket cum Invoice.\nThanks & Regards\nOmniPass',
-      attachments: [{ filename: 'Ticket.pdf', content: pdf }],
+    const pdfDoc = pdfMake.createPdf(docDefinition);
+    pdfDoc.getBuffer(async (buffer) => {
+      // Send the email with the PDF file attached
+      const info = await transporter.sendMail({
+        from: '"OmniPass" <help.omnipass@gmail.com>',
+        to: response.email,
+        subject: 'Ticket cum Invoice',
+        text: 'Dear Sir/Madam,\nThanks for booking your ticket on OmniPass.\nPlease find attached your Ticket cum Invoice.\nThanks & Regards\nOmniPass',
+        attachments: [{ filename: fileName, content: buffer }],
+      });
+      console.log('Message sent: %s', info.messageId);
     });
-
-    console.log('Message sent: %s', info.messageId);
   } catch (error) {
     console.error(error);
   }
