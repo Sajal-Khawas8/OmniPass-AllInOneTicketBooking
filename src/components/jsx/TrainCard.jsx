139680a8c4cc1083950c87536ee1f6bb0ebb6f8b
Sajal <sajalkhawas8@gmail.com>
2023-04-19 21:56:54 +0530

    Changed key

--- src/components/jsx/TrainCard.jsx
+++ src/components/jsx/TrainCard.jsx
@@ -11,7 +11,7 @@ export default function TrainCard(props) {
         const options = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': 'e7aeee0ebbmsh0e8880b3a016942p1a3dbbjsn40e9221762fb',
+                'X-RapidAPI-Key': '300a1a5fe9msh4b15ef5e2e5e86dp17eb52jsn74638ab4c79a',
                 'X-RapidAPI-Host': 'irctc1.p.rapidapi.com'
             }
         };
